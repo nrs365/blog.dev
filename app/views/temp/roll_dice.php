82d7b03eb190563a155ed123f425cb6f9c6a0d7b
nrs365 <nrs365@gmail.com>
2014-06-26 14:32:55 -0500

    view file to show random number and user guess

---
+++ app/views/temp/roll_dice.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Roll dice</title>
+</head>
+<body>
+    <h1>The random number is<?= $random; ?></h1>
+    <h1>Your guess was <?= $guess; ?></h1>
+</body>
+</html>
