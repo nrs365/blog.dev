17babac4b2fde94e866fa26a02cbe105321d4a85
nrs365 <nrs365@gmail.com>
2014-06-26 14:54:44 -0500

    added code to compare the guessed number and the random number

--- app/views/temp/roll_dice.php
+++ app/views/temp/roll_dice.php
@@ -4,7 +4,15 @@
     <title>Roll dice</title>
 </head>
 <body>
-    <h1>The random number is<?= $random; ?></h1>
-    <h1>Your guess was <?= $guess; ?></h1>
+    <!-- <h1>The random number is <?= $random; ?></h1>
+    <h1>Your guess was <?= $guess; ?></h1> -->
+
+    The random dice roll was: <?= $random; ?><br>
+    Your guess was: <?= $guess; ?><br>
+    <? if ($random == $guess) : ?>
+        <p style="color:green;">Your guess was correct!</p>
+    <? else : ?>
+        <p style="color:red;">Sorry, please try again!</p>
+    <? endif; ?>    
 </body>
 </html>
