fa2dede7cdeb228bfebe10042727f80f379a9b3d
nrs365 <nrs365@gmail.com>
2014-06-27 10:13:41 -0500

    refactored code to remove php shorthand tags and use blade instead

---
+++ app/views/temp/roll_dice.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+    <!-- <h1>The random number is <?= $random; ?></h1>
+    <h1>Your guess was <?= $guess; ?></h1> -->
+
+    The random dice roll was: <?= $random; ?><br>
+    Your guess was: <?= $guess; ?><br>
+    @if ($random == $guess)
+        <p style="color:green;">Your guess was correct!</p>
+    @else
+        <p style="color:red;">Sorry, please try again!</p>
+    @endif 
+@stop
