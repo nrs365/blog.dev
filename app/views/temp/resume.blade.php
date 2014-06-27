34c10c68ada88117cd02ab9ed34bf8b31cc1489b
nrs365 <nrs365@gmail.com>
2014-06-27 09:55:27 -0500

    refactored resume.html for use in laravel

---
+++ app/views/temp/resume.blade.php
@@ -0,0 +1,170 @@
+@extends('layouts.master')
+
+@section('topscript')
+<head>
+    <title>Resume page</title>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="/css/g1jquubbld/css/bootstrap.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="/js/bootstrap.min.js"></script>
+</head>
+@stop
+
+@section('content')
+<body>
+    <!--fixed navigation bar at the top of the page -->
+    <div id="wrap">
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar">Icon</span>
+                        <span class="icon-bar">Icon</span>
+                        <span class="icon-bar">Icon</span>
+                        <span class="icon-bar">Icon</span>
+                    </button>
+                    <a class="navbar-brand" href="/">My Blog</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class>
+                            <a href="/portfolio">Home</a>
+                        </li>   
+                        <li class="">
+                            <a href="/portfolio">Portfolio</a>
+                        </li>   
+                        <li class="active">
+                        <a href="/resume">Resume</a>
+                        </li>
+                    <ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!--Name and title container-->
+<div class="container">
+    <div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+        Nicole 
+        <small>Junior Software Developer</small>
+        </h1>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-lg-2 col-lg-offset-1">
+            <h5>CONTACT</h5>
+        </div>
+        <div class="col-lg-6">
+            <address>
+                <strong>Nicole S.</strong>
+                <br>
+                <a href="mailto:#">nrs365@gmail.com</a>
+            </address>
+        </div>
+        <div class="col-lg-3">
+            <p>
+            <a href="#">LinkedIn</a>
+            <br>
+            <a href="http://www.github.com/nrs365">GitHub</a>
+            </p>
+            </div>
+            <hr>
+        </div>
+        <div class="row">
+            <div class="col-lg-2 col-lg-offset-1">
+                <h5>WORK EXPERIENCE</h5>
+            </div>
+            <div class="col-lg-6">
+                <p>
+                    <strong>Best Buy</strong>
+                    <br>
+                    Wireless Lead; Mobile Sales Consultant
+                </p>
+            </div>
+        <div class="col-lg-3">
+            <p>October, 2007 - August, 2010</p>
+        </div>
+        <div class="col-lg-6 col-lg-offset-3">
+            <p>
+                <strong>Private tutor</strong>
+                <br>
+                Tutor English Second Language student; 4th and 5th grade
+            </p>
+            </div>
+            <div class="col-lg-3">
+                <p>March, 2013 - Present</p>
+            </div>
+        </div>
+        <div class="col-lg-6 col-lg-offset-3">
+            <p>
+                <strong>add</strong>
+                <br>
+                add
+            </p>
+            </div>
+            <div class="col-lg-3">
+                <p>add</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-2 col-lg-offset-1">
+                <h5>EDUCATION AND ACADEMICS</h5>
+            </div>
+            <div class="col-lg-6">
+                <p>
+                    <strong>Texas A&M Univeristy at San Antonio</strong>
+                    <br>
+                    Master's of Arts in English Literature
+                    Thesis
+                </p>
+            </div>
+        <div class="col-lg-3">
+            <p>August, 2010 - August, 2012</p>
+        </div>
+        <div class="col-lg-6 col-lg-offset-3">
+            <p>
+                <strong>University of Texas at San Antonio</strong>
+                <br>
+                Bachelor of Art Degree in English, Philosophy (double major)
+            </p>
+            </div>
+            <div class="col-lg-3">
+                <p>August, 2004 - 2010</p>
+            </div>
+        </div>
+        <div class="col-lg-6 col-lg-offset-3">
+            <p>
+                <strong>South Atlantic Modern Language Association (SAMLA) Conference, Atlanta, GA</strong>
+                <br>
+                Title of the paper of HW
+            </p>
+            </div>
+            <div class="col-lg-3">
+                <p>November, 2011</p>
+            </div>
+        </div>
+        <div class="col-lg-6 col-lg-offset-3">
+            <p>
+                <strong></strong>
+                <br>
+            Confrences and academics
+            </p>
+            </div>
+            <div class="col-lg-3">
+                <p>March, 2013 - Present</p>
+            </div>
+        </div>
+    </div>
+@stop
+</body>
+</html>
