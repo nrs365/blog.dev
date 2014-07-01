61685eb3af571e6b8b95a5bfcc3d0bc7fa6279dd
nrs365 <nrs365@gmail.com>
2014-07-01 11:05:56 -0500

    controllers for posts with database; incomplete - show not working correctly

---
+++ app/controllers/PostsController.php
@@ -0,0 +1,101 @@
+<?php
+
+class PostsController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$posts = Post::all();
+		return View::make('posts.index')->with('posts', $posts);
+		//return View::make('posts.index')->with('posts', Post::all());
+		
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		// $post = new Post();
+		// $post->title = "title";
+		// $post->body = "body";
+		// $post->save();
+		return View::make('posts.create');
+	}
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$validator = Validator::make(Input::all(), Post::$rules);
+		if($validator->fails())
+		{
+			return Redirect::back()->withInput()->withErrors($validator);
+		
+		} else {
+			$post = new Post();
+			$post->title = Input::get('title');
+			$post->body = Input::get('body');
+			$post->save();
+			return Redirect::action('PostsController@index');
+			//Log::info(Input::all());
+			//return Redirect::back()->withInput();
+		}
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$post = Post::find($id);
+		return View::make('posts.show')->with('post', $post);
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		return "Posts:edit()";
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		return "Posts:update()";
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		return "Posts::destroy()";
+	}
+}
