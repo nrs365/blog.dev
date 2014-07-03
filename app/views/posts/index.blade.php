dab05d64fd0daa299fa6d147db20b61b88f27647
nrs365 <nrs365@gmail.com>
2014-07-03 13:17:36 -0500

    added search bar as a form input

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,8 +2,29 @@
 
 @section('content')
     <h1>Posts</h1>
-    @foreach ($posts as $post)
-        {{ link_to_action('PostsController@show', $post->title, array($post->id)) }}
+    {{ link_to_action('PostsController@create', 'Create', array('class' => 'btn btn-default btn-lrg')) }}
+    <table class="table table-striped">
+        <tr>
+            <th>Title</th>
+            <th>Creation Date</th>
+            <th>Actions</th>
+        </tr>    
+        @foreach ($posts as $post)
+        <tr>
+            <td>{{ link_to_action('PostsController@show', $post->title, array($post->id)) }}</td>
+            <td>{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}</td>
+            <td>{{ link_to_action('PostsController@edit', 'Edit', array($post->id), array('class' => 'btn btn-default btn-sm')) }}</td>
+        </tr>
         <br>       
-    @endforeach
+        @endforeach
+    </table>
+    {{ $posts->links() }}
+    <div>
+        {{ Form::open(array('action'=>'PostsController@index', 'class' => 'form-inline', 'method' => 'GET')) }}
+            {{ Form::label('search', 'Search') }}
+            {{ Form::text('search', null, array('placeholder' => 'Search Query', 'class' => 'form-control col-lg-4')) }}
+            {{ Form::submit('Search') }} 
+        {{ Form::close() }} 
+    </div><br>
+
 @stop
