a62d1526359b8ab877ee16476c8769a65bce9fc3
nrs365 <nrs365@gmail.com>
2014-07-07 18:08:27 -0500

    migrations for the blog project

---
+++ app/database/migrations/2014_06_30_191501_create_posts_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+// class CreatePostsTable extends Migration {
+
+// 	/**
+// 	 * Run the migrations.
+// 	 *
+// 	 * @return void
+// 	 */
+// 	public function up()
+// 	{
+// 		//
+// 	}
+
+// 	/**
+// 	 * Reverse the migrations.
+// 	 *
+// 	 * @return void
+// 	 */
+// 	public function down()
+// 	{
+// 		//
+// 	}
+
+// }
+class CreatePostsTable extends Migration {
+
+    public function up()
+    {
+        Schema::create('posts', function($table)
+        {
+            $table->increments('id');
+            $table->string('title', 100);
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+
+}
