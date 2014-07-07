4b5a37078fc33c91aaade7911931c358ed95d1c0
nrs365 <nrs365@gmail.com>
2014-07-07 18:28:14 -0500

    added routes for login views

--- app/routes.php
+++ app/routes.php
@@ -14,35 +14,88 @@ Route::get('/', function (){
     return View::make('temp.my_first_view');
 });
 
-Route::get('/resume', function () {
-    return View::make('temp.resume');
+// Route::get('/resume', function () {
+//     return View::make('temp.resume');
+// });
+//
+// Route::get('/portfolio', function () {
+//     return View::make('temp.portfolio');
+// });
+
+Route::resource('/posts', 'PostsController');
+Route::get('/login', 'HomeController@showLogin');
+Route::post('/login', 'HomeController@doLogin');
+Route::get('/logout', 'HomeController@logout');
+
+route::get('/carousel', function () {
+    return View::make('temp.carousel');
 });
 
-Route::get('/portfolio', function () {
-    return View::make('temp.portfolio');
-});
+Route::get('/resume', 'HomeController@showResume');
+Route::get('/portfolio', 'HomeController@showPortfolio');
 
-Route::get('/sayhello/{name}', function($name)
-{
-    if ($name == "Chris")
-    {
-        return Redirect::to('/');
-    }
-    else
-    {
-        $data = array('name' => $name);
-        return View::make('temp.my_first_view')->with($data);
-    }
-});
 
-Route::get('/roll_dice/{guess}', function($guess) {
-    $random = rand(1,6);
-    $data = array (
-        'random' => $random,
-        'guess' => $guess    
-    );
-    return View::make('temp.roll_dice')->with($data);
-});
+// Route::get('/', 'HomeControllers@showWelcome');
+Route::get('/sayhello/{name}', 'HomeController@sayHello');
+
+
+
+// Route::get('/orm-test', function() {
+//     $post2 = new Post();
+//     $post2->title = "Newer Blog Post";
+//     $post2->body = "Eloquent uses PDO";
+//     $post2->save();
+//     return "Eloquent ORM is Eloquent";
+// });
+
+// Route::get('/orm-test', function () {
+//     $posts = Post::all();
+
+//     foreach($posts as $post) {
+//         echo $post->title . "<br>";
+//         echo $post->body . "<br>";
+//     }
+// });
+
+// Route::get('/orm-test', function() {
+//     $post = Post::find(1);
+
+//     echo $post->title . "<br>";
+//     echo $post->body . "<br>";
+//     $post->title = "This is a new title";
+//     $post->save();
+
+// });
+
+// Route::get('/orm-test', function() {
+//     $post = Post::find(1); // can use findOrFail as an alternative to find
+//     $post->delete();
+//     return "Eloquent ORM is eloquent";
+// });
+
+//Route::resource('create', 'PostsController');
+
+// Route::get('/sayhello/{name}', function($name)
+// {
+//     if ($name == "Chris")
+//     {
+//         return Redirect::to('/');
+//     }
+//     else
+//     {
+//         $data = array('name' => $name);
+//         return View::make('temp.my_first_view')->with($data);
+//     }
+// });
+
+// Route::get('/roll_dice/{guess}', function($guess) {
+//     $random = rand(1,6);
+//     $data = array (
+//         'random' => $random,
+//         'guess' => $guess    
+//     );
+//     return View::make('temp.roll_dice')->with($data);
+// });
 
 // Route::get('/sayHello/{named}', function($name)
 // {
