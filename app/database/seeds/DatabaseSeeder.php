2e9b55541758f822319d88870dc78b48d0a72662
nrs365 <nrs365@gmail.com>
2014-07-07 18:11:08 -0500

    added user info for seeding database table for blog project

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,40 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('PostTableSeeder');
 	}
+}	
 
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        $user = new User();
+        $user->email = 'admin@codeup.com';
+        $user->password = Hash::make('adminPass123!');
+        $user->save();
+   	}    
 }
+
+class PostTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('posts')->delete();
+
+		$user = User::first();
+
+		for ($i = 1; $i <= 10; $i++)
+		{
+			$post = new Post();
+			$post->user_id =$user->id;
+			$post->title = "Post Title $i";
+			$post->body = "Post Body $i";
+			$post->save();
+		}	
+	}
+}
+
