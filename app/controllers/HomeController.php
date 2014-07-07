47f558124e7162356817039349d0a7676d276b46
nrs365 <nrs365@gmail.com>
2014-07-07 18:00:40 -0500

    added login authentication to HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,9 +15,58 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
-	{
-		return View::make('hello');
+
+	public function showResume() {
+		$data = ['title' => 'resume page'];
+		return View::make('temp.resume')->with($data);
 	}
 
+	public function showPortfolio() {
+		return View::make('temp.portfolio');
+	}
+
+	public function showLogin() {
+		return View::make('login');
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function doLogin() {
+		$email = Input::get('email');
+		$password = Input::get('password');
+
+		if (Auth::attempt(array('email' => $email, 'password' => $password), Input::has('remember')))
+		{
+    		Session::flash('info_message', 'You have logged in successfully.');
+    		return Redirect::intended(action('PostsController@index'));
+		}
+		else
+		{
+    		Session::flash('errorMessage', 'Email or password was not found.');
+    		return Redirect::action('HomeController@showLogin')->withInput();
+		}
+	}
+
+	public function logout() {
+		Auth::logout();
+		Session::flash('info_message', 'You have logged out.');
+		return Redirect::action('PostsController@index');
+	}
+	
+
+	// public function showWelcome()
+	// {
+	// 	//return View::make('hello');
+	// 	return Redirect::action('HomeController@sayHello', ['Codeup']);
+	// }
+    
+ //    public function sayHello($name) {
+ //        $data = array('name' => $name);
+ //        	return View::make('temp.my_first_view')->with($data);
+ //    	}
+	// });
 }
+
