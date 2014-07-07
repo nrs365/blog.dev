239f6880b89370f20061a7824479db9f822b61b0
nrs365 <nrs365@gmail.com>
2014-07-07 18:45:45 -0500

    added functionality for viewing images that were loaded to posts, logging in, and logging out to create a post

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,29 +2,32 @@
 
 @section('content')
     <h1>Posts</h1>
-    {{ link_to_action('PostsController@create', 'Create', array('class' => 'btn btn-default btn-lrg')) }}
+    {{ link_to_action('PostsController@create', 'Create', null, array('class' => 'btn btn-success btn-lrg pull-right')) }}
     <table class="table table-striped">
         <tr>
             <th>Title</th>
+            <th>Author Email</th>
             <th>Creation Date</th>
             <th>Actions</th>
         </tr>    
         @foreach ($posts as $post)
         <tr>
             <td>{{ link_to_action('PostsController@show', $post->title, array($post->id)) }}</td>
+            <td>{{ $post->user->email }}
             <td>{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}</td>
             <td>{{ link_to_action('PostsController@edit', 'Edit', array($post->id), array('class' => 'btn btn-default btn-sm')) }}</td>
         </tr>
         <br>       
         @endforeach
     </table>
-    {{ $posts->links() }}
+    {{ $posts->appends(['search' => Input::get('search')])->links() }}
     <div>
-        {{ Form::open(array('action'=>'PostsController@index', 'class' => 'form-inline', 'method' => 'GET')) }}
-            {{ Form::label('search', 'Search') }}
+        {{ Form::open(array('action'=>'PostsController@index', 'class' => 'form-inline', 'method' => 'GET', 'files' => true)) }}
+            {{ Form::label('search') }}
             {{ Form::text('search', null, array('placeholder' => 'Search Query', 'class' => 'form-control col-lg-4')) }}
-            {{ Form::submit('Search') }} 
+            {{ Form::submit('Search', array('class' => 'btn btn-success')) }} 
         {{ Form::close() }} 
+        {{-- $posts->appends(['search' => Input::get('search')]) --}}
     </div><br>
 
 @stop
