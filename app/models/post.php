240363c498f5e74af5535dbabda7cb62b8145f42
nrs365 <nrs365@gmail.com>
2014-07-07 18:21:18 -0500

    added function for user uploaded images

---
+++ app/models/post.php
@@ -0,0 +1,26 @@
+<?php
+
+class Post extends BaseModel {
+    //the db table this model relates to
+    protected $table = 'posts';
+    protected $imgDir = 'img_upload';
+    //validation rules for our model properties
+    static public $rules = [
+        'title' => 'required|max:100',
+        'body' => 'required'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
+    public function addUploadedImage($image) {
+        $systemPath = public_path() . '/' . $this->imgDir . '/';
+        $imageName = $this->id . '-' . $image->getClientOriginalName();
+        $image->move($systemPath, $imageName);
+        $this->img_path = '/' . $this->imgDir . '/' . $imageName;
+        
+    }
+
+}
