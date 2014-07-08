d574aa1921d9deef1295c5d1ba3f33145dbdf517
nrs365 <nrs365@gmail.com>
2014-07-08 09:03:37 -0500

    updated show view for user uploaded images with posts and login authentication for user

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <h1>{{{ $post->title }}}</h1>
-    <p>{{{ $post->created_at }}}</p>
+    <h5>{{{ $post->user->email }}}</h5>
+    <h5>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</h5>
     <p>{{{ $post->body }}}</p>
+    @if ($post->img_path)
+        <img src="{{{ $post->img_path }}}" class="responsive">
+    @endif    
+
+    {{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method'=> 'DELETE')) }}
+        {{ Form::submit('Delete') }}
+    {{ Form::close() }}
 @stop
